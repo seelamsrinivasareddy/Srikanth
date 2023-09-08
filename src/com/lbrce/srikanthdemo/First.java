1753effa1b5a4cf82b266cd12b97f57345199c0f
ML Lab <ML Lab@MLLAB01>
2023-09-08 10:15:43 +0530

    First Commit

---
+++ src/com/lbrce/srikanthdemo/First.java
@@ -0,0 +1,14 @@
+package com.lbrce.srikanthdemo;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		fun();
+	}
+	public static void fun()
+	{
+		System.out.println("This is Fun() from Srinu");
+	}
+
+}
